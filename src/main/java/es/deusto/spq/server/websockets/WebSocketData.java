638c1c6d9c3e3bfa07043ef8e4cf11346bc1d166
Alvaro <78875373+AlvaroSuarezGomez@users.noreply.github.com>
2023-05-01 20:30:30 +0200

    Revert "Revert "Encoders y decoders de WebSockets comenzados""
    This reverts commit 09835d36583d6144fb50fb161cbb6cec8ff80cde.
    
    Co-Authored-By: Lander Gallastegi <LNDF@users.noreply.github.com>

---
+++ src/main/java/es/deusto/spq/server/websockets/WebSocketData.java
@@ -0,0 +1,40 @@
+package es.deusto.spq.server.websockets;
+
+public abstract class WebSocketData {
+	
+	private String type;
+	
+	protected WebSocketData(String type) {
+		this.type = type;
+	}
+	
+	public static WebSocketData decode(String str) {
+		int lineEnd = str.indexOf("\n");
+		String type = str.substring(0, lineEnd);
+		String data = str.substring(lineEnd + 1, str.length());
+		
+		if (type.equals("Send")) {
+			return WebSocketSendData.decodeData(data);
+		} else if (type.equals("Receive")) {
+			return WebSocketReceiveData.decodeData(data);
+ 		} else if (type.equals("Join")) {
+ 			return WebSocketJoinData.decodeData(data);
+ 		} else if (type.equals("Leave")) {
+ 			return WebSocketLeaveData.decodeData(data);
+ 		} else if (type.equals("History")) {
+ 			return WebSocketHistoryData.decodeData(data);
+ 		} else {
+ 			return null;
+ 		}
+	}
+	
+	public abstract String encode();
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
---
+++ src/main/java/es/deusto/spq/server/websockets/WebSocketSendData.java
@@ -0,0 +1,18 @@
+package es.deusto.spq.server.websockets;
+
+public class WebSocketSendData extends WebSocketData {
+
+	public WebSocketSendData() {
+		super("Send");
+	}
+	
+	@Override
+	public String encode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static WebSocketSendData decodeData(String data) {
+		return null;
+	}
+}
