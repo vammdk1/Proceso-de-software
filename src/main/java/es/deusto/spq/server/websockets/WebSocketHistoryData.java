2898c648753e64aaee1ab2d8c0c937beae8befff
Alvaro <78875373+AlvaroSuarezGomez@users.noreply.github.com>
2023-05-01 20:30:30 +0200

    Revert "Revert "Encoders y decoders de WebSockets comenzados""
    This reverts commit 09835d36583d6144fb50fb161cbb6cec8ff80cde.

---
+++ src/main/java/es/deusto/spq/server/websockets/WebSocketHistoryData.java
@@ -0,0 +1,18 @@
+package es.deusto.spq.server.websockets;
+
+public class WebSocketHistoryData extends WebSocketData {
+
+	public WebSocketHistoryData() {
+		super("History");
+	}
+	
+	@Override
+	public String encode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public static WebSocketHistoryData decodeData(String data) {
+		return null;
+	}
+}
