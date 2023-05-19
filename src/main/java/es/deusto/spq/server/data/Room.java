929f43e4feb4d9d68bf664ae30c1006337507a31
Alvaro <78875373+AlvaroSuarezGomez@users.noreply.github.com>
2023-05-19 19:43:02 +0200

    Actualización de WebSockets en la parte servidor
    Co-Authored-By: Lander Gallastegi <LNDF@users.noreply.github.com>

--- src/main/java/es/deusto/spq/server/data/Room.java
+++ src/main/java/es/deusto/spq/server/data/Room.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import es.deusto.spq.server.jdo.User;
+import es.deusto.spq.server.websockets.WebSocketHistoryData;
 import es.deusto.spq.server.websockets.WebSocketLeaveData;
 import es.deusto.spq.server.websockets.WebSocketReceiveData;
 
@@ -62,6 +63,15 @@ public class Room {
 			Message message = new Message(user.getLogin() + " ha entrado en la sala.", "SYSTEM");
 			this.addMessage(message);		
 			users.put(user, session);
+			if (session.isOpen()) {
+				WebSocketHistoryData historyData = new WebSocketHistoryData(messages);
+				try {
+					session.getRemote().sendString(historyData.encode());
+				} catch (IOException e) {
+					logger.error("Error broadcasting messages");
+					e.printStackTrace();
+				}
+			}
 			return true;
 		}
 	}
@@ -72,7 +82,7 @@ public class Room {
 		}
 		WebSocketLeaveData data = new WebSocketLeaveData();
 		try {
-			is (users.get(usuario).isOpen()) {
+			if (users.get(usuario).isOpen()) {
 				users.get(usuario).getRemote().sendString(data.encode());
 			}
 		} catch (IOException e) {
