bd784e9d232d45f03706feebdde2e0fc190fcc1f
LNDF <landergallastegi@gmail.com>
2023-05-20 18:09:17 +0200

    Import...

--- src/test/java/es/deusto/testing/JDO/JDOTest.java
+++ src/test/java/es/deusto/testing/JDO/JDOTest.java
@@ -1,7 +1,6 @@
 package es.deusto.testing.JDO;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
