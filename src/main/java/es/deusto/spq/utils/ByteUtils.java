9a10d5480e193c985c1e8ec78210ed4163879a9c
LNDF <landergallastegi@gmail.com>
2023-05-24 01:36:47 +0200

    Me acabo de fumar un porro

---
+++ src/main/java/es/deusto/spq/utils/ByteUtils.java
@@ -0,0 +1,32 @@
+package es.deusto.spq.utils;
+
+public class ByteUtils {
+    
+    public static byte[] byteArrayToBits(byte[] bytes) {
+        byte[] bits = new byte[(bytes.length + 7) / 8];
+        for (int i = 0; i < bytes.length; i++) {
+            if (bytes[i] != 0) {
+                bits[bits.length - i / 8 - 1] |= 1 << (i % 8);
+            }
+        }
+        for (int i = 0; i < bits.length / 2; i++) {
+            bits[i] ^= bits[bits.length - i - 1];
+            bits[bits.length - i - 1] ^= bits[i];
+            bits[i] ^= bits[bits.length - i - 1];
+        }
+        return bits;
+    }
+
+    public static byte[] bitArrayToBytes(byte[] bits) {
+        byte[] bytes = new byte[bits.length * 8];
+        for (int i = 0; i < bytes.length; i++) {
+            if ((bits[i / 8] & 1 << (i % 8)) != 0) {
+                bytes[i] = 1;
+            } else {
+                bytes[i] = 0;
+            }
+        }
+        return bytes;
+    }
+
+}
